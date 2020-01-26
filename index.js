358186073b819e441563986d911a13be51b22271
Ayush Shrivastav <42120573+AyushShri@users.noreply.github.com>
2020-01-26 09:02:44 +0530

    Added Server side / index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var express=require('express');
+var app = express()
+
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
+app.get('/', function(req,res){
+
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection',function(socket){
+    console.log("A user connection is detected");
+    socket.on('chat message',function(msg){
+        io.emit('chat message',msg);
+        console.log('message:'+msg);
+    
+    });
+});
+http.listen(4000,function(){
+
+console.log("Listening on the port");
+});
